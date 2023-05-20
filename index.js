e30d19a6230f9a00b4257d9c0122fa9110183771
CraftPur-Official <rinku.apnatourandtravel@gmail.com>
2023-05-20 15:01:02 +0530

    updated the arrow

--- index.js
+++ index.js
@@ -12,23 +12,31 @@ function expand(element) {
 
     if (wrapper.classList.contains('active')) {
         wrapper.classList.toggle('active');
+        questionDiv.children[1].classList.remove('fa-caret-up');
+        questionDiv.children[1].classList.add('fa-caret-down');
         setTimeout(() => {
             answerDiv.classList.toggle('hide');
         }, 100);
+        console.log('clicked an opened accordian');
     }
     else {
         answerDiv.classList.toggle('hide');
+        questionDiv.children[1].classList.remove('fa-caret-down');
+        questionDiv.children[1].classList.add('fa-caret-up');
         setTimeout(() => {
             wrapper.classList.toggle('active');
         }, 100);
-    }
-
-    for (let i = 0; i < wrapper.parentElement.children.length; i++) {
-        if (i !== Array.from(wrapper.parentElement.children).indexOf(wrapper)) {
-            wrapper.parentElement.children[i].classList.remove('active');
-            setTimeout(() => {
-                wrapper.parentElement.children[i].children[1].classList.add('hide');
-            }, 100);
+        
+        for (let i = 0; i < wrapper.parentElement.children.length; i++) {
+            if (i !== Array.from(wrapper.parentElement.children).indexOf(wrapper)) {
+                wrapper.parentElement.children[i].classList.remove('active');
+                wrapper.parentElement.children[i].children[0].children[1].classList.add('fa-caret-down');
+                wrapper.parentElement.children[i].children[0].children[1].classList.remove('fa-caret-up');
+                setTimeout(() => {
+                    wrapper.parentElement.children[i].children[1].classList.add('hide');
+                }, 100);
+            }
         }
+        console.log('clicked a closed accordian')
     }
 }
